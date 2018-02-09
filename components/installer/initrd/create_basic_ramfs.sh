f03e7ea93aefa89a73ed535268c2d0a84b8aae41
KansassCityShuffle <lisa.aubry@insa-cvl.fr>
2018-02-09 22:17:41 +0100

    create testing ramfs

--- components/installer/initrd/create_basic_ramfs.sh
+++ components/installer/initrd/create_basic_ramfs.sh
@@ -4,17 +4,21 @@ set -o errexit
 set -o nounset 
 set -o pipefail
 
-source ../../config/default.conf  
+source ../../../config/default.conf  
 
 # Initialize target
 TARGET="${INITRD_IN}/initrd"
 [[ -d $TARGET ]] && rm -rf $TARGET
 mkdir $TARGET
 
-# Create filesystem structure
-mkdir -p ${TARGET}/{dev,dev,etc,lib,lib64,mnt/root,proc,root,sbin,sys}
+# Initramfs
+mkdir -p ${TARGET}/{bin,dev,dev,etc,lib,lib64,mnt/root,proc,root,sbin,sys}
 cp -a /dev/{null,console,tty,sda1} ${TARGET}/dev 
 
-# Add utilities 
+# Utilities 
+cp -a ${REPOSITORY}/busybox ${TARGET}/bin/ 
+
+# Init script 
+cp -a  ${INITRD}/init ${TARGET}/
 
 exit 0
