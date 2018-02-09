a318fa41122fe92bdb6fad1dedb991faec6c3539
KansassCityShuffle <lisa.aubry@insa-cvl.fr>
2018-02-09 21:19:23 +0100

    initramfs - utilities

--- components/installer/initrd/create_basic_ramfs.sh
+++ components/installer/initrd/create_basic_ramfs.sh
@@ -4,7 +4,17 @@ set -o errexit
 set -o nounset 
 set -o pipefail
 
-source ../../config/default.conf
- 
+source ../../config/default.conf  
+
+# Initialize target
+TARGET="${INITRD_IN}/initrd"
+[[ -d $TARGET ]] && rm -rf $TARGET
+mkdir $TARGET
+
+# Create filesystem structure
+mkdir -p ${TARGET}/{dev,dev,etc,lib,lib64,mnt/root,proc,root,sbin,sys}
+cp -a /dev/{null,console,tty,sda1} ${TARGET}/dev 
+
+# Add utilities 
 
 exit 0
