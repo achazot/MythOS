6956e44fd7066a0294e5c1451c271ddfc5ebad53
achazot <alban.chazot@insa-cvl.fr>
2018-02-09 20:18:42 +0100

    avancement

--- build/build.sh
+++ build/build.sh
@@ -2,6 +2,22 @@
 
 # TODO: source common functions
 
+# Get the script absolute path
+OWN_DIR=$(dirname "$(readlink -f "$0")")
+REL_CONF_PATH="../config/default.conf"
+
+if [ ! -f "$OWN_DIR/$REL_CONF_PATH" ]; then
+    printf "Missing config file $OWN_DIR/$REL_CONF_PATH, aborting.\n"
+    exit 1
+fi
+
+# Read configuration variables
+# No, I won't check for malicious code here
+source "$OWN_DIR/$REL_CONF_PATH"
+
+
+
+
 # TODO: write usage
 usage ()
 {
@@ -15,7 +31,11 @@ EOF
 
 main ()
 {
+    # Copy installer system
+    cp -ar "$ISOLINUX_IN" "$ISOLINUX_OUT"
+    cp -ar "$KERNEL_IN" "$INSTALL_OUT"
     
+
 }
 
 
