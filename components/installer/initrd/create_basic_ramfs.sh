e266a18315f0dfe07588105d4cd005b7a1b8f62e
KansassCityShuffle <lisa.aubry@insa-cvl.fr>
2018-02-09 22:30:08 +0100

    pizza

--- components/installer/initrd/create_basic_ramfs.sh
+++ components/installer/initrd/create_basic_ramfs.sh
@@ -1,3 +1,7 @@
+# Resulting initramfs must be packaged as a compressed cpio archive 
+# For embedded it directly in the kernel image set 
+# CONFIG_INITIRAMFS_SOURCE=<path_to_initramfs> before compiling the kernel 
+
 #!/bin/bash 
 
 set -o errexit
