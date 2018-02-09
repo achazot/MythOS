949ee3a0ab0dcf8b7bf00b7e17a81e93bea37161
achazot <alban.chazot@insa-cvl.fr>
2018-02-09 20:00:05 +0100

    debut script build

---
+++ build/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# TODO: source common functions
+
+# TODO: write usage
+usage ()
+{
+    cat <<-EOF
+    build.sh -- Generate an iso
+    Usage:
+        ./build.sh
+EOF
+}
+
+
+main ()
+{
+    
+}
+
+
+
+
+main $@
+exit 0
